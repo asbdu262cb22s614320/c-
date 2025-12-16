fac8376eeeaa7a6f5ce2778dc8ba0cc8dc0cdf91
Jasmine Minisha <jasmineminisha2005@gmail.com>
2025-12-16 11:49:24 +0430

    basic set1

---
+++ Basic set/Program.cs
@@ -0,0 +1,247 @@
+﻿using System;
+
+namespace Demo32
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+/*
+            Console.WriteLine("Enter The Number 1");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter The Number 2");
+            int n2 = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine(n1 + n2);
+                        Console.WriteLine(n1 - n2);
+                        Console.WriteLine(n1 * n2);
+
+   */
+     
+           Console.WriteLine("enter the value a");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("ente the value b");
+            int b = int.Parse(Console.ReadLine());
+
+      
+            Console.WriteLine(Math.Pow(a,b));
+              
+
+          /*  Console.WriteLine("enter your length");
+            long                   length =
+            long.Parse(Console.ReadLine());
+            Console.WriteLine("ente your weight");
+            float                    weight =
+            float.Parse(Console.ReadLine());
+            Console.WriteLine("enter your height");
+            double height =
+            double.Parse(Console.ReadLine());
+
+            Console.WriteLine("your length is" + length);            
+            Console.WriteLine("your weight is" + weight);
+            Console.WriteLine("your height is" + height);
+
+
+            Console.WriteLine(length * weight * height);
+            */
+/*
+            Console.WriteLine("enter the value");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("ente your pi value");
+            float              pi =
+            float.Parse(Console.ReadLine());
+            Console.WriteLine("enter your radius");
+            double radius =
+            double.Parse(Console.ReadLine());
+
+            Console.WriteLine("your value is"+ a);
+            Console.WriteLine("your pi is"+pi);
+            Console.WriteLine("your radius is"+radius);
+
+          Console.WriteLine(2 * pi * radius);
+             
+        /*    Console.WriteLine("ente your numerator");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter your denominator");
+            int d = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("your numerator is"+n);
+            Console.WriteLine("your denominator is"+d);
+
+            Console.WriteLine(n / d );
+            Console.WriteLine(d / n);
+            */
+      /*   Console.WriteLine("enter the value ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("your value is");
+            Console.WriteLine(Math.Pow(a,a));*/
+           /* 
+            Console.WriteLine("enter the number");
+            int r = int.Parse(Console.ReadLine());
+            Console.WriteLine("your number is"+ r);
+           Console.WriteLine(Math.Sqrt(25));
+            */
+     /*     Console.WriteLine("enter the value b");
+          int b = int.Parse(Console.ReadLine());
+          Console.WriteLine("enter the value h");
+          int h = int.Parse(Console.ReadLine());
+          Console.WriteLine(1 / 2 * (b * h));
+            */
+         /*   Console.WriteLine(" enter the km");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("your km" + a);
+            Console.WriteLine(a * 0.621371);
+            */
+    /*        Console.WriteLine("enter the celsius value c");
+            int c = int.Parse(Console.ReadLine());
+            Console.WriteLine("your celsius value"+c);
+            Console.WriteLine(c * 9 / 5+ 32);
+            */
+         /*  Console.WriteLine("enter the value a");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the value b");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the temp value");
+            int temp = int.Parse(Console.ReadLine());
+            Console.WriteLine(a = b);
+            Console.WriteLine(b = temp);
+           */
+      /*   Console.WriteLine("enter the value a");
+            int a=int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the value b");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine(a = a + b);
+            Console.WriteLine(b = a - b);
+            Console.WriteLine(a = a - b);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            */
+     /*       Console.WriteLine("ente the value a");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("ente the value b");
+            int b = int.Parse(Console.ReadLine());
+            a = a ^ b;
+            b = a ^ b;
+            a = a ^ b;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            */
+            /*
+            Console.WriteLine("enter the result");
+            int result = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine(result);
+           */
+/*
+
+          Console.WriteLine("enter the length");
+          double length=
+         double.Parse(Console.ReadLine());
+          Console.WriteLine("enter the width");
+          double width =
+        double.Parse(Console.ReadLine());
+          Console.WriteLine((length * width) / 43560);
+          */
+
+
+
+                      
+       /*     Console.WriteLine("enter the number");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(n * (n + 1) / 2);
+            */
+/*
+            Console.WriteLine("ente the number");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the number");
+            int n2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the number");
+            int n3 = int.Parse(Console.ReadLine());
+            Console.WriteLine((n1 + n2 + n3) / 3);
+            */
+
+          /*  Console.WriteLine("enter the billamount");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine(b * 5 / 100);
+            */
+           /*
+            Console.WriteLine("enter the basic pay");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the DA");
+            int da = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the TA");
+            int ta = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("enter the PF");
+            int pf = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(b * da / 100);
+            Console.WriteLine(b * ta / 100);
+            Console.WriteLine(b * pf / 100);
+            Console.WriteLine(b + da + ta - pf);
+            */
+
+       /*     Console.WriteLine("enter the value");
+            int p = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the value");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the value");
+            int r = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("your value is" + p);
+            Console.WriteLine("your value is" + n);
+            Console.WriteLine("your value is" + r);
+            
+            Console.WriteLine(p * n * r / 100);
+            */
+            /*
+           Console.WriteLine("enter the principle");
+            int p = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the rate");
+            int r = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the numberr");
+            int n= int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the time");
+            int t = int.Parse(Console.ReadLine());
+            Console.WriteLine(p * ((1 + r / n) ^ (n*t)));
+            */
+            
+           /* Console.WriteLine("enter the height");
+            int h = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the inches");
+            int i = int.Parse(Console.ReadLine());
+            Console.WriteLine(h / 2.54);
+           */
+/*
+           Console.WriteLine("enter the height");
+            double height=
+             double.Parse(Console.ReadLine());
+            Console.WriteLine("enter the weight");
+            double weight=
+          double.Parse(Console.ReadLine());
+             Console.WriteLine(weight/height*height); 
+           */
+
+        /*    Console.WriteLine("enter the roll no");
+            int r = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the name");
+            string name = Console.ReadLine();
+            Console.WriteLine("enter the tamil mark");
+            int t = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the english mark");
+            int e = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the maths mark");
+            int m = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(t + e + m / 3);
+            */
+  
+
+  
+    
+
+
+            }
+        
+    }
+}
